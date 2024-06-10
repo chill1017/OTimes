3858531c25549d234e22c51997238fb97be9dd61
Caleb Hill <ckh1017@wildcats.unh.edu>
2024-06-10 12:01:03 -0400

    old changes

--- graph_18b/hom0to1.m
+++
@@ -1,4 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-{<|"scalar" -> 0, "p" -> 2, "q" -> ee, "s" -> 2, "t" -> 2|>, 
- <|"scalar" -> 0, "p" -> 3, "q" -> jj, "s" -> 3, "t" -> 3|>, 
- <|"scalar" -> 0, "p" -> 4, "q" -> oo, "s" -> 4, "t" -> 4|>}
--- graph_18b/hom1to0.m
+++
@@ -1,4 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-{<|"scalar" -> 0, "p" -> ee, "q" -> 2, "s" -> 2, "t" -> 2|>, 
- <|"scalar" -> 0, "p" -> jj, "q" -> 3, "s" -> 3, "t" -> 3|>, 
- <|"scalar" -> 0, "p" -> oo, "q" -> 4, "s" -> 4, "t" -> 4|>}
