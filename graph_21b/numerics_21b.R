11925faf11a2a9c0163befb299e4983d4dfe441a
Caleb Hill <ckh1017@wildcats.unh.edu>
2024-07-17 15:37:33 -0400

    basics, and bigon relations

--- graph_21b/numerics_21b.R
+++ graph_21b/numerics_21b.R
@@ -1,12 +1,12 @@
 library("BB")
 library(tidyverse)
 
-setwd("/Users/calebhill/Documents/UNH/Research/code/github/graph_21b/")
+setwd("/Users/calebhill/Documents/UNH/Research/code/OTimes/graph_21b/")
   
 
 n <- 22  # number of parameters
-k <- 100 #number of different start points
-p0 <- matrix(runif(n*k, -2, 2), k, n)
+k <- 5 #number of different start points
+p0 <- matrix(runif(n*k, -.5, .5), k, n)
 ans <- multiStart(par=p0, fn=g21b_Sys, action="optimize", quiet=TRUE) 
 result <- as_tibble(ans) %>% filter(converged == TRUE) %>% filter(fvalue < 1)
 
